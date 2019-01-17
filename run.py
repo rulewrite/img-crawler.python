c262591dce2c8016a13e73d8e2a1a3b835ca2202
rulewrite <rulewrite@gmail.com>
2019-01-18 02:59:44 +0900

    Save finally img (temp img down to  merge)

--- run.py
+++ run.py
@@ -1,39 +1,85 @@
 import os
 import requests
 from bs4 import BeautifulSoup
+import urllib.request
+from PIL import Image as PILImage
 
 # init
+# todo 인자받기
 name = ''
 baseUrl = ''
 # todo baseUrl 끝에 / 있음 자르기
 basePath = ''
-
-startParam = '3'
-endParam = '189'
-
 imgSelector = ''
 
-outputDir = '~/' + name
-outputImgFileNameBase = name + '_img_'
+outputDir = './' + name + '/'
+outputImgFileNameBase = outputDir + name + '_img_%d%s'
 
+startParam = 3
+endParam = 6 # 안썻을때 req status보고 200 !== 시 정지, 셀릭터 10회 이상 비면 정지
+endParam += 1
 
-# todo 반복시작
+# start logic
 
-# HTTP GET request
-url = baseUrl + basePath
-req = requests.get(url % startParam)
+def getExtension(filename):
+    ext = '.'.join(filename.split('.')[1:])
+    return '.' + ext if ext else '.jpg'
 
-# get HTML soruce
-html = req.text
+# todo mkdir
 
-# html.parsing
-soup = BeautifulSoup(html, 'html.parser')
+pageNum = 1
+for i in range (startParam, endParam):
+    print(str(pageNum) + '화 저장중...')
 
-# select
-imgs = soup.select(imgSelector)
+    ext = None
+    tempImgArr = []
+    fullWidth, fullHeight = 0, 0
 
-for img in imgs:
-    src = img.get('src')
-    getSrc = src if ('//' in src) else baseUrl + src
-    # todo 이미지 다운, 병합, 저장
-    print(getSrc)
+    # HTTP GET request
+    url = baseUrl + basePath
+    req = requests.get(url % startParam) # todo !== 200 에러
+    # get HTML soruce
+    html = req.text
+
+    # html.parsing
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # loop tempImg
+    for imgTag in soup.select(imgSelector):
+        src = imgTag.get('src')
+        imgUrl = src if ('//' in src) else baseUrl + src
+        imgName = os.path.basename(imgUrl)
+        tempImgFileName = outputDir + imgName
+
+        # extention
+        if not ext:
+            ext = getExtension(imgName)
+
+        # img save
+        urllib.request.urlretrieve(imgUrl, tempImgFileName)
+        # todo 파일명 중복시?
+        # 엑박 이미지일 경우?
+
+        tempImg = PILImage.open(tempImgFileName)
+        width , height = tempImg.size
+        tempImgArr.append(tempImg)
+        # set outputImg max width
+        fullWidth = max(fullWidth, width)
+        # stack outputImg height
+        fullHeight += height
+        # img remove
+        os.remove(tempImgFileName)
+
+    # merge now page in img
+    canvas = PILImage.new('RGB', (fullWidth, fullHeight), 'white')
+    pasteHeightPosition = 0
+    for tempImg in tempImgArr:
+        width, height = tempImg.size
+        canvas.paste(tempImg, (0, pasteHeightPosition))
+        pasteHeightPosition += height
+    
+    # save
+    canvas.save(outputImgFileNameBase % (pageNum, 'jpg'))
+
+    print(str(pageNum) + '화 저장완료')
+    pageNum += 1
