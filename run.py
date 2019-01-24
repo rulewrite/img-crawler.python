fa00a51caaab80a0c18cca1b9cff6cc67d9ac57e
rulewrite <rulewrite@gmail.com>
2019-01-25 03:04:54 +0900

    url of nextpage from element href attr, if final img height > 65500px split save
    다음페이지의 url을 element에 href 속성 사용 가능, 최종 저장 이미지의 세로 길이가 65500px 이상일 경우 분할 저장
    
    추가기능
    - url 패턴이 일정치 않은 경우 기존 param 변수를 활용할 수 없는 상황대처
    
    버그수정
    - jpeg 파일의 경우 65500px 이상 저장 불가
    - imgRes 변수에서 reqCheck 함수로 request 받고 except 처리까지 한 res(line 128)를 사용하지 않고 다시 request(line 137)하는 부분 수정

--- run.py
+++ run.py
@@ -6,28 +6,43 @@ from PIL import Image as PILImage
 
 '''
 TODO: list
-- 속도개선 (이미지 병합 부분, 불필요한 저장부분, PIL 삭제 > requiements.txt 최신화)
+- 속도개선 (이미지 병합 부분, 불필요한 저장부분, PIL 삭제 :: readlines() > requiements.txt 최신화)
 - 인자 받기
 - ui 툴로 컨버팅
 - url parameter로 처리 불가한 웹페이지 (next 버튼)
 - 모듈화, 분할
+- err log 텍스트 파일
 '''
 
-#-------- init
-baseUrl = ''
-if (baseUrl[-1:] == '/'): baseUrl = baseUrl[:-1]
-basePath = ''
-if (basePath[0] != '/'): basePath = '/' + basePath
+sameFileNamePass = True
+name = ''
+
+urlOrigin = ''
+urlPath = ''
 imgSelector = ''
 
-name = ''
+startParam = None
+endParam = None
+nextSelector = ''
+
+#-------- init
+loopType = None
+if not startParam is None:
+    loopType = 'param'
+elif not nextSelector is None:
+    loopType = 'selector'
+else:
+    print('ERROR:: Require parameter not defiend')
+    # quit()
+    sys.exit(1)
+
+if (urlOrigin[-1:] == '/'): urlOrigin = urlOrigin[:-1]
+if (urlPath[0] != '/'): urlPath = '/' + urlPath
+
 outputDir = './' + name + '/'
 outputImgFileNameBase = outputDir + name + '_img_%d'
 
-startParam = 3
-endParam = 4
-
-#-------- start logic
+#-------- function declaration
 def getExtension(filename):
     ext = '.'.join(filename.split('.')[1:])
     return '.' + ext if ext else '.jpg'
@@ -52,6 +67,21 @@ def getUniqueFileName(filename, ext):
         filename = filename % idx
     return filename + ext
 
+def getAbsoulteRoute(route):
+    return route if '//' in route else urlOrigin + route
+
+def saveImg(imgArr, fullWidth, fullHeight, filename):
+    canvas = PILImage.new('RGB', (fullWidth, fullHeight), 'white')
+    pasteHeightPosition = 0
+    for img in imgArr:
+        height = img.size[1]
+        canvas.paste(img, (0, pasteHeightPosition))
+        pasteHeightPosition += height
+    canvas.save(filename)
+
+#-------- start logic
+print('=' * 30)
+
 # mkdir
 try:
     if not (os.path.isdir(outputDir)):
@@ -59,70 +89,100 @@ try:
         os.makedirs(outputDir)
 except OSError as err:
     print('ERROR:: Failed to make directory: ', err)
-    # quit()
     sys.exit(1)
 
 # loop start
 resultMsg = ''
 fileNum = 0
-nowParam = (startParam-1)
 selectorEmptyCnt, pageReqErrCnt, imgReqErrUrl = 0, 0, False
+
+nowUrl = None
+nowParam = startParam
+nextElements = None
 def breakCheck():
     global resultMsg
     isContinue = True
     if (imgReqErrUrl != False):
         resultMsg = 'BREAK:: %d page(param: %d) in image name "%s" request error' % (fileNum, nowParam, imgReqErrUrl)
         isContinue = False
+    elif (selectorEmptyCnt >= 5):
+        resultMsg = 'BREAK:: Selector empty count (%d).' % selectorEmptyCnt
+        isContinue = False
+    elif (pageReqErrCnt >= 5):
+        resultMsg = 'BREAK:: Page request error count (%d).' % pageReqErrCnt
+        isContinue = False
     else:
-        if (endParam):
-            if (nowParam >= endParam):
+        global nowUrl
+        if (loopType == 'param'):
+            if (not endParam is None and nowParam >= endParam):
                 resultMsg = 'DONE:: Page loop is end. Done.'
                 isContinue = False
-        else:
-            if (selectorEmptyCnt >= 5):
-                resultMsg = 'BREAK:: Selector empty count (%d).' % selectorEmptyCnt
-                isContinue = False
-            elif (pageReqErrCnt >= 5):
-                resultMsg = 'BREAK:: Page request error count (%d).' % pageReqErrCnt
-                isContinue = False
+            else:
+                nowUrl = urlOrigin + urlPath % nowParam
+        elif (loopType == 'selector'):
+            global nextElements
+            # init
+            if nextElements is None:
+                nowUrl = urlOrigin + urlPath
+            else:
+                # href를 찾고 nextElements를 False 시킨뒤
+                if (nextElements is False or not len(nextElements)):
+                    resultMsg = 'DONE:: Next element is empty'
+                    isContinue = False
+                else:
+                    found = False
+                    for nextEl in nextElements:
+                        nextElHref = nextEl.get('href')
+                        if not nextElHref is None:
+                            '''
+                            FIXME:
+                            - if href is not url
+                                - javascript:alert('is Last.')
+                                - #id
+                            - click event
+                            '''
+                            nowUrl = getAbsoulteRoute(nextElHref)
+                            found = True
+                            break
+                        
+                    if found:
+                        nextElements = False
+                    else:
+                        resultMsg = 'BREAK:: cat\'t found attr "href"'
+                        isContinue = False
     return isContinue
 
 while breakCheck():
     print('-'*30)
     fileNum += 1
-    nowParam += 1
-    outputImgFileName = outputImgFileNameBase % fileNum
+    ext = None
+    outputImgFileNameYetExt = outputImgFileNameBase % fileNum
+    thisPagePass = False
     print('%d page crawling' % fileNum)
 
     # HTTP GET request
-    url = baseUrl + basePath
-    res = reqCheck(url % nowParam)
+    res = reqCheck(nowUrl)
     if not res:
         print('|- Page request error. go to next')
         pageReqErrCnt += 1
         continue
 
     # html.parsing
-    imgTags = BeautifulSoup(res.text, 'html.parser').select(imgSelector)
-    if not (len(imgTags)):
+    bs = BeautifulSoup(res.text, 'html.parser')
+    imgTags = bs.select(imgSelector)
+    if not len(imgTags):
         print('|- Selector empty result. go to next')
         selectorEmptyCnt += 1
         continue
 
     # loop tempImg
     print('|- found %d images' % len(imgTags))
-    ext = None
     tempImgArr = []
     fullWidth, fullHeight = 0, 0
     for imgTag in imgTags:
         src = imgTag.get('src')
         # relative, absoulte root
-        imgUrl = src if ('//' in src) else baseUrl + src
-
-        # set extention
-        if not ext: ext = getExtension(os.path.basename(src))
-
-        tempImgFileName = getUniqueFileName(outputImgFileName + '_temp', ext)
+        imgUrl = getAbsoulteRoute(src)
 
         # get img
         imgRes = reqCheck(imgUrl)
@@ -131,37 +191,59 @@ while breakCheck():
             imgReqErrUrl = imgUrl
             break
 
+        # set extention
+        if ext is None: 
+            ext = getExtension(os.path.basename(src))
+            # already file pass
+            if sameFileNamePass and os.path.isfile(outputImgFileNameYetExt + ext):
+                thisPagePass = True
+                break
+        tempImgFileName = getUniqueFileName(outputImgFileNameYetExt + '_temp_%d' % len(tempImgArr), ext)
+
         # append img
         with open(tempImgFileName, 'wb') as f:
             # img write
-            f.write(requests.get(imgUrl).content)
-            
-            tempImg = PILImage.open(tempImgFileName)
-            width, height = tempImg.size
-            tempImgArr.append(tempImg)
-            # set outputImg max width
-            fullWidth = max(fullWidth, width)
-            # stack outputImg height
-            fullHeight += height
-            
-            # img remove
-            os.remove(tempImgFileName)
+            f.write(imgRes.content)
 
+            try:
+                tempImg = PILImage.open(tempImgFileName)
+                width, height = tempImg.size
+                tempImgArr.append(tempImg)
+
+                # stack outputImg height
+                fullHeight += height
+                if (fullHeight > 65500):
+                    print('| %d page is too large so divide save' % fileNum)
+                    fullHeight -= height
+                    saveImg(tempImgArr[:-1], fullWidth, fullHeight, getUniqueFileName(outputImgFileNameYetExt, ext))
+                    tempImgArr = [tempImg]
+                    fullHeight = height
+                    fullWidth = 0
+                
+                # set outputImg max width
+                fullWidth = max(fullWidth, width)
+
+                # img remove
+                os.remove(tempImgFileName)
+            except:
+                print('ERROR:: Pillow Err outputfileNmae: "%s", tempImgName: "%s"' % (outputImgFileNameYetExt, tempImgFileName))
+
+    if (thisPagePass):
+        print('CONTINUE:: %d page is already' % fileNum)
+        continue
+    
     if (len(tempImgArr)):
         # merge imgs in page
-        canvas = PILImage.new('RGB', (fullWidth, fullHeight), 'white')
-        pasteHeightPosition = 0
-        for tempImg in tempImgArr:
-            width, height = tempImg.size
-            canvas.paste(tempImg, (0, pasteHeightPosition))
-            pasteHeightPosition += height
-        
-        # save
-        canvas.save(getUniqueFileName(outputImgFileName, ext))
-
+        saveImg(tempImgArr, fullWidth, fullHeight, getUniqueFileName(outputImgFileNameYetExt, ext))
         print('|- saved')
     else:
         print('|- empty images. unsaved')
+
+    # prevent next url
+    if (loopType == 'param'):
+        nowParam += 1
+    elif (loopType == 'selector'):
+        nextElements = bs.select(nextSelector)
 
 resultMsg += '\nloop total: %d' % fileNum
 print('='*30)
